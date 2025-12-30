b4457ab81d0540572bdd10bc73bb17285a5ce360
Aisha Imtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2025-12-30 16:37:37 +0530

    Update task01_createCounter.js

--- closures/task01_createCounter.js
+++ closures/task01_createCounter.js
@@ -1,1 +1,25 @@
 // TODO: Implement this task
+const counter = {
+  let counter =0;
+return {
+  increament(){
+    return ++counter;
+  },
+  decreament(){
+    if(counter>0)
+     return --counter;
+    return 0;
+  },
+  reset(){
+    counter = 0;
+    return 0;
+  }
+
+}
+
+const myCounter = counter();
+console.log(myCounter.increament());
+console.log(myCounter.increament());
+console.log(myCounter.increament());
+console.log(myCounter.decreament());
+console.log(myCounter.reset());
